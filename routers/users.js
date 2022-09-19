f002b99fc03cbefbbddb64fc6bbe92548d8bdbc1
apurba <apurbadehidar@outlook.com>
2022-09-19 21:25:06 +0530

    refactor

--- routers/users.js
+++ routers/users.js
@@ -1,66 +1,16 @@
 import express from 'express';
-import { v4 as uuidv4 } from 'uuid';
+import {addUser,getUserByID,deleteUserByID,updateUserByID,getAllUsers} from '../controllers/users.js'
  
 const router = express.Router();
  
-let users = [];
+router.get('/',getAllUsers);
 
-router.get('/',(req,res)=>{
-    res.send(users);
-})
+router.post("/",addUser);
 
-router.post("/",(req,res)=>{ 
+router.get("/:id", getUserByID);
 
-    const newUser = {...req.body,  id  : uuidv4()} ;
+router.delete("/:id",deleteUserByID);
 
-    users.push(newUser);
-
-    res.status(201).json({success: true, payload: newUser})
-});
-
-router.get("/:id", (req, res)=>{
-
-    const searchUserID = req.params.id; 
-
-    const searchUser = users.find(user => user.id === searchUserID);
-
-    if(searchUser){
-        res.status(200).json({success : true, payload : searchUser});
-    }
-    else{
-        res.status(401).json({success : false, payload : 'No user found'});
-    }
-    
-});
-
-router.delete("/:id",(req,res)=>{
-
-    const userID = req.params.id; 
-
-    users = users.filter(user => user.id !== userID);
-
-    res.status(200).json({success : true, payload : users});
-
-
-});
-
-router.patch(("/:id"),(req,res)=>{
-
-    const userID = req.params.id; 
-
-    const user = users.find(user => user.id === userID);
-
-    const {firstName,lastName,age} = req.body; 
-
-    if(firstName) user.firstName = firstName;
-
-    if(lastName) user.lastName = lastName;
-
-    if(age) user.age = age;
-
-    res.status(200).json({success : true, payload : user});
-
-
-})
+router.patch(("/:id"),updateUserByID)
 
 export default router;
